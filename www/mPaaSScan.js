3bcc2eb6814e071a46c6e926e5068982bcdc9d79
e-zhanghai <e-zhanghai@geely.com>
2020-12-12 18:56:26 +0800

    优化

--- www/mPaaSScan.js
+++ www/mPaaSScan.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.standardScan = function (options, success, error) {
+exports.scan = function (options, success, error) {
     exec(success, error, 'mPaaSScan', 'standardScan', [options]);
 };
